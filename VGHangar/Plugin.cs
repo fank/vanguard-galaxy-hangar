c209e9fc94f41385933c9be90dbad72cebf85218
Florian Kinder <florian.kinder@fankserver.com>
2026-06-05 22:15:02 +0200

    chore: bump version to 0.1.1 for the 0.8.1.4 arrow fix (#2)

--- VGHangar/Plugin.cs
+++ VGHangar/Plugin.cs
@@ -13,7 +13,7 @@ public class Plugin : BaseUnityPlugin
     public const string PluginName = "Vanguard Galaxy Hangar";
     // BepInEx parses PluginVersion through System.Version which rejects SemVer
     // pre-release suffixes, so stick to the plain N.N.N form.
-    public const string PluginVersion = "0.1.0";
+    public const string PluginVersion = "0.1.1";
 
     internal static Plugin Instance { get; private set; } = null!;
     internal static ManualLogSource Log { get; private set; } = null!;
